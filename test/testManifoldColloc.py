fix a serious lqr Jacobian problem

test/testManifoldColloc.py
@@ -18,7 +18,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import logging
 sys.path.append('../')
-from trajOptLib.fakeio import getOnOffArgs
+from trajOptLib.io import getOnOffArgs
 from trajOptLib import daeSystem, trajOptCollocProblem
 from trajOptLib import manifoldConstr, trajOptManifoldCollocProblem
 from trajOptLib import nonLinearPointConstr
@@ -31,6 +31,9 @@ from scipy.sparse import coo_matrix
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+RX = 0
+RY = 0
+RADIUS = 1
 
 
 class SimpleOmniCar(daeSystem):
@@ -57,15 +60,18 @@ class OmniCar(daeSystem):
     """
     def __init__(self):
         daeSystem.__init__(self, 6, 2, 1, 2, 8)
+        self.rx = RX
+        self.ry = RY
+        self.radius = RADIUS
 
     def dyn(self, t, x, u, p, y, G, row, col, rec, needg):
-        y[0] = x[4] - u[0] - 2 * p[0] * (x[0] - 0.5)
-        y[1] = x[5] - u[1] - 2 * p[0] * (x[1] - 0.5)
+        y[0] = x[4] - u[0] - p[0] * (x[0] - self.rx)
+        y[1] = x[5] - u[1] - p[0] * (x[1] - self.ry)
         if needg:
             G[0:2] = 1.0
             G[2:4] = -1.0
             G[4:6] = -p[0]  # w.r.t. x[0], x[1]
-            G[6:8] = [-2*(x[0] - 0.5), -2*(x[1] - 0.5)]  # w.r.t p
+            G[6:8] = [-(x[0] - self.rx), -(x[1] - self.ry)]  # w.r.t p
             if rec:
                 row[:] = [0, 1, 0, 1, 0, 1, 0, 1]
                 col[:] = [5, 6, 7, 8, 1, 2, 9, 9]
@@ -84,20 +90,23 @@ class collocCircleConstr(nonLinearPointConstr):
                 nG += 6
                 nf += 1
         nonLinearPointConstr.__init__(self, -1, nf, 6, 2, nG=nG)
+        self.x = 0
+        self.y = 0
+        self.r = 1
 
     def __callg__(self, X, F, G, row, col, rec, needg):
         x, y, dx, dy, ddx, ddy = X[1:7]
-        F[0] = (x-0.5)**2 + (y-0.5)**2 - 0.5
+        F[0] = (x-self.x)**2 + (y-self.y)**2 - self.r**2
         if self.with_vel:
-            F[1] = (x-0.5)*dx + (y-0.5)*dy
+            F[1] = (x-self.x)*dx + (y-self.y)*dy
             if self.with_acc:
-                F[2] = dx*dx + (x-0.5)*ddx + dy*dy + (y-0.5)*ddy
+                F[2] = dx*dx + (x-self.x)*ddx + dy*dy + (y-self.y)*ddy
         if needg:
-            G[:2] = [2*(x-0.5), 2*(y-0.5)]
+            G[:2] = [2*(x-self.x), 2*(y-self.y)]
             if self.with_vel:
-                G[2:6] = [dx, dy, x - 0.5, y - 0.5]
+                G[2:6] = [dx, dy, x - self.x, y - self.y]
                 if self.with_acc:
-                    G[6:12] = [ddx, ddy, 2*dx, 2*dy, x - 0.5, y - 0.5]
+                    G[6:12] = [ddx, ddy, 2*dx, 2*dy, x - self.x, y - self.y]
             if rec:
                 row[:2] = [0, 0]
                 col[:2] = [1, 2]
@@ -113,15 +122,18 @@ class circleConstr(manifoldConstr):
     def __init__(self, with_vel=False):
         manifoldConstr.__init__(self, 6, 1, 2, constr_order=0, nG=12, nnzJx=2, nnzJg=2)
         self.with_vel = with_vel
+        self.x = RX
+        self.y = RY
+        self.r = RADIUS
 
     def __callg__(self, X, F, G, row, col, rec, needg):
         """Calculate the constraints at knot points."""
         x, y, dx, dy, ddx, ddy = X
-        F[0] = (x-0.5)**2 + (y-0.5)**2 - 0.5
-        F[1] = (x-0.5)*dx + (y-0.5)*dy
-        F[2] = dx*dx + (x-0.5)*ddx + dy*dy + (y-0.5)*ddy
+        F[0] = (x-self.x)**2 + (y-self.y)**2 - self.r**2
+        F[1] = (x-self.x)*dx + (y-self.y)*dy
+        F[2] = dx*dx + (x-self.x)*ddx + dy*dy + (y-self.y)*ddy
         if needg:
-            G[:] = [2*(x-0.5), 2*(y-0.5), dx, dy, x - 0.5, y - 0.5, ddx, ddy, 2*dx, 2*dy, x - 0.5, y-0.5]
+            G[:] = [2*(x-self.x), 2*(y-self.y), dx, dy, x - self.x, y - self.y, ddx, ddy, 2*dx, 2*dy, x - self.x, y-self.y]
             if rec:
                 row[:] = [0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2]
                 col[:] = [0, 1, 0, 1, 2, 3, 0, 1, 2, 3, 4, 5]
@@ -130,10 +142,10 @@ class circleConstr(manifoldConstr):
         """Calculate the constraint J(q)^T \gamma and corresponding Jacobians"""
         x, y, dx, dy, ddx, ddy = X
         gamma = Gamma[0]
-        Y[0] = 2 * (x - 0.5) * gamma
-        Y[1] = 2 * (y - 0.5) * gamma
+        Y[0] = 2 * (x - self.x) * gamma
+        Y[1] = 2 * (y - self.x) * gamma
         if needg:
-            Gg[:] = [2*(x-0.5), 2*(y-0.5)]
+            Gg[:] = [2*(x-self.x), 2*(y-self.y)]
             Gq[:] = [2*gamma, 2*gamma]
             if rec:
                 rowg[:] = [0, 1]
@@ -143,93 +155,167 @@ class circleConstr(manifoldConstr):
 
 
 def main():
-    args = getOnOffArgs('car', 'debug', 'naive')
+    args = getOnOffArgs('car', 'debug', 'naive', 'analy', 'warm')
     if args.debug:
         sys.path.append('/home/motion/GAO/ThirdPartyLibs/pycharm-debug.egg')
         import pydevd
-        pydevd.settrace('10.197.47.90', port=10000, stdoutToServer=True, stderrToServer=True)
+        pydevd.settrace('10.197.52.193', port=10000, stdoutToServer=True, stderrToServer=True)
     if args.naive:
         testNaiveCar()
     if args.car:
-        testCar()
+        testCar(warm=args.warm)
+    if args.analy:
+        testAnalytic()
 
 
-def testCar():
-    """Test the omni-directional car problem."""
-    # sys = OmniCar()
-    sys = SimpleOmniCar()
+def testAnalytic():
+    """Test car problem with analytic solution. See what's missing."""
+    sys = SimpleOmniCar()  # simple car has no p
     N = 10
     t0 = 0
     tf = 1
     man_constr = circleConstr()
     prob = trajOptManifoldCollocProblem(sys, N, t0, tf, man_constr)
-    prob.xbd = [-1e20*np.ones(6), 1e20*np.ones(6)]
-    prob.ubd = [-1e20*np.ones(2), 1e20*np.ones(2)]
-    # prob.pbd = [np.zeros(1), 1e20*np.ones(1)]
-    x0bd = np.array([0, 0, 0, 0, -1e20, -1e20])
-    prob.x0bd = [x0bd, -x0bd]
-    xflb = np.array([1, 1, 0, 0, -1e20, -1e20])
-    xfub = np.array([1, 1, 0, 0, 1e20, 1e20])
-    prob.xfbd = [xflb, xfub]
+    setBound(prob)
+
     lqr = lqrObj(R=np.ones(2))
     prob.addLQRObj(lqr)
-    prob.preProcess()
+    prob.preProcess(defect_u=False, defect_p=False)
     # construct solver
     cfg = snoptConfig()
     cfg.printLevel = 1
     cfg.printFile = 'test.out'
-    cfg.verifyLevel = 3
+    cfg.verifyLevel = 1
     slv = solver(prob, cfg)
-    rst = slv.solveRand()
-    if True:  #rst.flag == 1 or rst.flag == 2:
-        psf = prob.parseF(rst.sol.copy())
-        print(psf['Gamma'])
+    # generate an guess
+    x0 = np.zeros(prob.nx)
+    # generate the random initial guess
+    t = np.linspace(0, 1, 2*N - 1)
+    theta = -np.pi * t ** 3 + 1.5 * np.pi * t ** 2
+    dtheta = -3*np.pi*t**2 + 3*np.pi*t
+    ddtheta = -6*np.pi*t + 3*np.pi
+    # assign them to it
+    useX, useU, useP = prob.__parseX__(x0)
+    radius = 1
+    stheta = np.sin(theta)
+    ctheta = np.cos(theta)
+    useX[:, 0] = radius * ctheta
+    useX[:, 1] = radius * stheta
+    useX[:, 2] = -radius * stheta * dtheta
+    useX[:, 3] = radius * ctheta * dtheta
+    useX[:, 4] = -radius * ctheta * dtheta ** 2 - radius * stheta * ddtheta
+    useX[:, 5] = -radius * stheta * dtheta ** 2 + radius * ctheta * ddtheta
+    useU[:, 0] = useX[:, 4]
+    useU[:, 1] = useX[:, 5]
+    # gamma is assumed zero
+    # set auxv
+    useAuxV = prob.__parseAuxVc__(x0)
+    useAuxV[:] = useX[1::2, 2:4]
+    psf0 = prob.parseF(x0)
+    rst = slv.solveGuess(x0)
+    psf = prob.parseF(rst.sol)
+    # parse the solution
+    sol = prob.parseSol(rst.sol)
+    showSol(sol)
+
+
+def testCar(warm=False):
+    """Test the omni-directional car problem."""
+    sys = OmniCar()
+    # sys = SimpleOmniCar()
+    N = 10
+    t0 = 0
+    tf = 0.9
+    man_constr = circleConstr()
+    prob = trajOptManifoldCollocProblem(sys, N, t0, tf, man_constr)
+    setBound(prob)
+
+    lqr = lqrObj(R=np.ones(2))
+    prob.addLQRObj(lqr)
+    prob.preProcess(defect_u=True, defect_p=False)
+    # construct solver
+    cfg = snoptConfig()
+    cfg.printLevel = 1
+    cfg.printFile = 'test.out'
+    cfg.verifyLevel = 1
+    slv = solver(prob, cfg)
+    if warm:
+        x0 = np.zeros(prob.numSol)
+        data = np.load('first_order_traj.npz')
+        useX, useU, useP = prob.__parseX__(x0)
+        useGamma = prob.__parseGamma__(x0)
+        useAuxVc = prob.__parseAuxVc__(x0)
+        useX[:] = data['X']
+        useU[:] = data['U']
+        useP[:] = data['P']
+        useGamma[:] = data['gamma']
+        useAuxVc[:] = useX[1::2, 2:4]
+        psf = prob.parseF(x0)
+        rst = slv.solveGuess(x0)
+    else:
+        rst = slv.solveRand()
+    if rst.flag == 1 or rst.flag == 2:
+        psf = prob.parseF(rst.sol)
+        np.savez('tmp.npz', **psf)
+        print(psf.keys())
+        print(psf['gamma'])
         # parse the solution
-        sol = prob.parseSol(rst.sol.copy())
+        sol = prob.parseSol(rst.sol)
         showSol(sol)
 
 
 def testNaiveCar():
     """Use ordinary approach and see"""
-    sys = OmniCar()
+    sys = SimpleOmniCar()
     N = 10
     t0 = 0
     tf = 1
-    man_constr = circleConstr()
     prob = trajOptCollocProblem(sys, N, t0, tf)
-    prob.xbd = [-1e20*np.ones(6), 1e20*np.ones(6)]
-    prob.ubd = [-1e20*np.ones(2), 1e20*np.ones(2)]
-    x0bd = np.array([0, 0, 0, 0, -1e20, -1e20])
-    prob.x0bd = [x0bd, -x0bd]
-    xflb = np.array([1, 1, 0, 0, -1e20, -1e20])
-    xfub = np.array([1, 1, 0, 0, 1e20, 1e20])
-    prob.xfbd = [xflb, xfub]
+    setBound(prob)
+
     lqr = lqrObj(R=np.ones(2))
     prob.addLQRObj(lqr)
+
     constr = collocCircleConstr(with_vel=True, with_acce=True)
     prob.addConstr(constr, path=True)
-    prob.preProcess()
+    prob.preProcess(colloc_constr_is_on=False, defect_p=False, defect_u=False)
     # construct solver
     cfg = snoptConfig()
     cfg.printLevel = 1
     cfg.printFile = 'test.out'
-    cfg.verifyLevel = 3
+    cfg.verifyLevel = 1
     slv = solver(prob, cfg)
     rst = slv.solveRand()
-    sol = prob.parseF(rst.sol.copy())
+    psf = prob.parseF(rst.sol.copy())
     if rst.flag == 1:
         # parse the solution
         sol = prob.parseSol(rst.sol.copy())
         x = sol['x']
-        fig, ax = plt.subplots()
-        ax.plot(x[:, 0], x[:, 1])
+        u = sol['u']
+        fig, ax = plt.subplots(2, 2)
+        ax[0][0].plot(x[:, 0], x[:, 1])
+        ax[0][1].plot(u[:, 0])
+        ax[0][1].plot(u[:, 1])
         # check velocity
-        velvio = (x[:, 0] - 0.5) * x[:, 2] + (x[:, 1] - 0.5) * x[:, 3]
+        velvio = (x[:, 0] - constr.x) * x[:, 2] + (x[:, 1] - constr.y) * x[:, 3]
         print(velvio)
-        accevio = x[:, 2] ** 2 + (x[:, 0] - 0.5) * x[:, 4] + x[:, 3] ** 2 + (x[:, 1] - 0.5) * x[:, 5]
+        accevio = x[:, 2] ** 2 + (x[:, 0] - constr.x) * x[:, 4] + x[:, 3] ** 2 + (x[:, 1] - constr.y) * x[:, 5]
         print(accevio)
         plt.show()
         # showSol(sol)
+
+
+def setBound(prob):
+    x0lb = np.array([1, 0, 0, 0, -1e20, -1e20])
+    x0ub = np.array([1, 0, 0, 0, 1e20, 1e20])
+    prob.x0bd = [x0lb, x0ub]
+    xflb = np.array([0, 1, 0, 0, -1e20, -1e20])
+    xfub = np.array([0, 1, 0, 0, 1e20, 1e20])
+    prob.xfbd = [xflb, xfub]
+    prob.xbd = [-1e20*np.ones(6), 1e20*np.ones(6)]
+    prob.ubd = [-1e20*np.ones(2), 1e20*np.ones(2)]
+    if prob.numP > 0:
+        prob.pbd = [-1e20*np.ones(1), 1e20*np.ones(1)]
 
 
 if __name__ == '__main__':

trajOptLib/trajOptCollocationProblem.py
@@ -136,22 +136,30 @@ class trajOptCollocProblem(probFun):
         self.t0ind, self.tfind = self.__getTimeIndices()
         self.colloc_constr_is_on = False
 
-    def preProcess(self, colloc_constr_is_on=False):
+    def preProcess(self, colloc_constr_is_on=False, defect_u=True, defect_p=True):
         """Initialize the instances of probFun now we are ready.
 
         Call this function after the objectives and constraints have been set appropriately.
         It calculate the space required for SNOPT and allocates sparsity structure if necessary.
 
         :param colloc_constr_is_on: bool, if we also impose constraints on those collocation points.
+        :param defect_u: bool, if we want to impose defect constraint on u, i.e. umid=(u0+uf)/2
+        :param defect_p: bool, if we want to impose defect constraint on p, i.e. pmid=(p0+pf)/2
 
         **Caveat** it might make problem over-constrained, if the path constraints are equality constraints.
 
         """
+        self.defectU = defect_u
+        self.defectP = defect_p
         self.colloc_constr_is_on = colloc_constr_is_on
         numDyn = self.dimdyn * self.nPoint  # constraints from system dynamics, they are imposed everywhere
         dynDefectSize = 2 * self.daeOrder * self.dimdyn
         self.dynDefectSize = dynDefectSize
-        defectSize = dynDefectSize + self.dimu + self.dimp  # from x, dx, and u, p are average
+        defectSize = dynDefectSize
+        if defect_u:
+            defectSize += self.dimu
+        if defect_p:
+            defectSize += self.dimp
         self.defectSize = defectSize
         numDefectDyn = (self.N - 1) * defectSize  # from enforcing those guys
         self.numDefectDyn = numDefectDyn
@@ -353,7 +361,7 @@ class trajOptCollocProblem(probFun):
         spA = coo_matrix((catA, (catArow, catAcol)))
         return spA, addn
 
-    def __setAPattern__(self, ndyncon, nnonlincon, spA, withp=True):
+    def __setAPattern__(self, ndyncon, nnonlincon, spA):
         """Set sparsity pattern from linear constraints and objective functions.
 
         It finds sparsity pattern from defect constraints, linear constraints, and objective functions.
@@ -370,10 +378,9 @@ class trajOptCollocProblem(probFun):
         :param ndyncon: int, describes how many dynamics constraints we have
         :param nnonlincon: int, describes how many nonlinear constraints we have
         :param spA: sparse matrix, how the objective function is described linearly.
-        :param withp: bool, if we set defect constraint on parameter p
 
         """
-        curRow, A, row, col = self.__setDefectPattern__(ndyncon, withp)
+        curRow, A, row, col = self.__setDefectPattern__(ndyncon, self.defectU, self.defectP)
         curRow += nnonlincon
         # we are ready to parse linear constraints
         lstCA, lstCArow, lstCAcol = self.__parseLinearConstraints__(curRow)
@@ -390,20 +397,23 @@ class trajOptCollocProblem(probFun):
         self.spA = csr_matrix((self.Aval, (self.Arow, self.Acol)), shape=(self.nf, self.nx))
         self.spA_coo = self.spA.tocoo()
 
-    def __setDefectPattern__(self, ndyncon, withp=True, withv=True):
+    def __setDefectPattern__(self, ndyncon, withu=True, withp=True):
         """Set the sparse linear constraints from defect constraints.
 
         :param ndyncon: number of dynamical constraints. This sets starting row.
+        :param withu: bool, determines if we set defect constraint on u
         :param withp: bool, determines if we set defect constraint on p
 
         """
         dimx, dimu, dimp, dimpoint, dimdyn = self.dimx, self.dimu, self.dimp, self.dimpoint, self.dimdyn
         if not withp:
             dimp = 0
+        if not withu:
+            dimu = 0
         if self.fixTimeMode:
-            lenA = (10*self.daeOrder*self.dimdyn + 3*(self.dimu + dimp)) * (self.N - 1)
+            lenA = (10*self.daeOrder*self.dimdyn + 3*(dimu + dimp)) * (self.N - 1)
         else:
-            lenA = (6*self.daeOrder*self.dimdyn + 3*(self.dimu + dimp)) * (self.N - 1)
+            lenA = (6*self.daeOrder*self.dimdyn + 3*(dimu + dimp)) * (self.N - 1)
         A = np.zeros(lenA)
         row = np.zeros(lenA, dtype=int)
         col = np.zeros(lenA, dtype=int)
@@ -436,19 +446,17 @@ class trajOptCollocProblem(probFun):
             righti = 2*(i + 1)
             A[curNA: curNA + 2*dimu] = 0.5
             A[curNA + 2*dimu: curNA + 3*dimu] = -1
-            if dimp > 0:
-                A[curNA + 3*dimu: curNA + 3 * dimu + 2*dimp] = 0.5
-                A[curNA + 3*dimu + 2 * dimp: curNA + 3*dimu + 3*dimp] = -1
+            A[curNA + 3*dimu: curNA + 3 * dimu + 2*dimp] = 0.5
+            A[curNA + 3*dimu + 2 * dimp: curNA + 3*dimu + 3*dimp] = -1
             row[curNA: curNA + 3 * dimu] = curRow + np.tile(np.arange(dimu), (3, 1)).flatten()
             col[curNA: curNA + dimu] = lefti * dimpoint + dimx + np.arange(dimu)
             col[curNA + dimu: curNA + 2 * dimu] = righti * dimpoint + dimx + np.arange(dimu)
             col[curNA + 2 * dimu: curNA + 3 * dimu] = midi * dimpoint + dimx + np.arange(dimu)
             curNA_ = curNA + 3 * dimu
-            if dimp > 0:
-                row[curNA_: curNA_ + 3 * dimp] = curRow + dimu + np.tile(np.arange(dimp), (3, 1)).flatten()
-                col[curNA_: curNA_ + dimp] = lefti * dimpoint + dimx + dimu + np.arange(dimp)
-                col[curNA_ + dimp: curNA_ + 2 * dimp] = righti * dimpoint + dimx + dimu + np.arange(dimp)
-                col[curNA_ + 2 * dimp: curNA_ + 3 * dimp] = midi * dimpoint + dimx + dimu + np.arange(dimp)
+            row[curNA_: curNA_ + 3 * dimp] = curRow + dimu + np.tile(np.arange(dimp), (3, 1)).flatten()
+            col[curNA_: curNA_ + dimp] = lefti * dimpoint + dimx + dimu + np.arange(dimp)
+            col[curNA_ + dimp: curNA_ + 2 * dimp] = righti * dimpoint + dimx + dimu + np.arange(dimp)
+            col[curNA_ + 2 * dimp: curNA_ + 3 * dimp] = midi * dimpoint + dimx + dimu + np.arange(dimp)
             curNA += 3 * (dimu + dimp)
             curRow += dimu + dimp
         return curRow, A, row, col
@@ -836,10 +844,11 @@ class trajOptCollocProblem(probFun):
         useP = X[:, self.dimpoint - self.dimp:]
         return useX, useU, useP
 
-    def parseF(self, guess):
+    def parseF(self, guess, y=None):
         """Give an guess, evaluate it and parse into parts.
 
         :param guess: ndarray, (numSol, ) a guess or a solution to check
+        :param y: ndarray, (numF, ) if None, it stores the solution
         :returns: dict, containing objective and parsed constraints
 
         """
@@ -848,16 +857,36 @@ class trajOptCollocProblem(probFun):
         nPoint = self.nPoint
         dimx = self.dimx
         dimdyn = self.dimdyn
-        y = np.zeros(self.numF)
+        if y is None:
+            y = np.zeros(self.numF)
         if self.grad:
             self.__callg__(guess, y, np.zeros(1), np.zeros(1), np.zeros(1), False, False)
         else:
             self.__callf__(guess, y)
+        y += self.spA.dot(guess)
         obj = y[0]
         dynCon = np.reshape(y[1:(2*N-1)*dimdyn+1], (2*N - 1, dimdyn))
         curN = 1 + (2 * N - 1) * dimdyn
-        curNf = curN + self.numDefectDyn
-        defectCon = np.reshape(y[curN: curNf], (N - 1, -1))
+        curNf = curN + self.dynDefectSize * (N - 1)
+        defectCon = dict()
+        dynDefectCon = np.reshape(y[curN: curNf], (N - 1, -1))
+        defectCon['dyn'] = dynDefectCon
+        curN = curNf
+        if self.defectU:
+            dimu = self.dimu
+        else:
+            dimu = 0
+        if self.defectP:
+            dimp = self.dimp
+        else:
+            dimp = 0
+        curNf += (dimu + dimp) * (N - 1)
+        upDefectCon = np.reshape(y[curN: curNf], (N - 1, -1))
+        if dimu > 0:
+            defectCon['u'] = upDefectCon[:, :dimu]
+        if dimp > 0:
+            defectCon['p'] = upDefectCon[:, dimu:dimu+dimp]
+
         curN = curNf
         pointCon = []
         for constr in self.pointConstr:
@@ -875,11 +904,12 @@ class trajOptCollocProblem(probFun):
         for constr in self.nonLinConstr:
             nonLinCon.append(y[curN: curN+constr.nf])
             curN += constr.nf
+        # all linear constraints can be ignored, here we ignore them
         # check bounds, return a -1, 1 value for non-equality bounds, and 0 for equality bounds
         useX, useU, useP = self.__parseX__(guess)
         Xbound = checkInBounds(useX[:, :dimx], self.xbd)
         x0bound = checkInBounds(useX[0, :dimx], self.x0bd)
-        xfbound = checkInBounds(useX[-1, :dimx], self.xfbd) # TODO: support acceleration bound check
+        xfbound = checkInBounds(useX[-1, :dimx], self.xfbd)
         ubound = checkInBounds(useU, self.ubd)
         if self.dimp > 0:
             pbound = checkInBounds(useP, self.pbd)
@@ -899,7 +929,8 @@ class trajOptCollocProblem(probFun):
         else:
             addXbound = None
         useX, useU, useP = self.__parseX__(guess)
-        rst = {'obj': obj, 'dyn': dynCon, 'defect': defectCon, 'point': pointCon, 'path': pathCon, 'nonlin': nonLinCon,
+        objs = guess[self.numSol - self.objaddn:]
+        rst = {'obj': obj, 'objs': objs, 'dyn': dynCon, 'defect': defectCon, 'point': pointCon, 'path': pathCon, 'nonlin': nonLinCon,
                 'Xbd': Xbound, 'Ubd': ubound, 'x0bd': x0bound, 'xfbd': xfbound, 'Pbd': pbound,
                 't0bd': t0bound, 'tfbd': tfbound, 'addXbd': addXbound,
                 'X': useX, 'U': useU, 'P': useP}
@@ -1018,7 +1049,7 @@ class trajOptCollocProblem(probFun):
             curRow += self.dimdyn
         # offset of row number due to defect dynamics constraint
         if self.fixTimeMode:
-            curRow += self.numDefectDyn 
+            curRow += self.numDefectDyn
         else:
             # manually set those defect dynamics and gradients, etc
             defectRow = (self.N - 1) * 2 * self.daeOrder * dimdyn
@@ -1065,7 +1096,10 @@ class trajOptCollocProblem(probFun):
                                     col[curNg: curNg + 2*dimdyn] = self.tfind
                                 curNg += 2*dimdyn
                     curRow += 2 * dimdyn
-            curRow += (self.N - 1) * (dimp + dimu)  # defect constraints on ctrl and parameters are linear
+            if self.defectU:
+                curRow += (self.N - 1) * self.dimu
+            if self.defectP:
+                curRow += (self.N - 1) * self.dimp
         return curRow, curNg
 
     def __copy_into_g__(self, index, G, row, col, curRow, curNg, nG, time_index, plus, rec,
@@ -1408,24 +1442,25 @@ class trajOptCollocProblem(probFun):
             yP = 0.0
             yTf = tfweight * (h * (self.N - 1))
             curG = 0
+            G_ = np.reshape(G[:num1], (nPoint, -1))  # use the same structure with col_
             if useQ > 0:
-                yQ = np.dot(weight[:, 0], np.sum(((useX[:, Qcol] - lqrobj.xbase[Qcol]) ** 2) * self.lqrobj.Q.data, axis=1)) * h
+                yQ = np.dot(weight[:, 0], np.sum(((useX[:, Qcol] - lqrobj.xbase[Qcol]) ** 2) * lqrobj.Q.data, axis=1)) * h
                 if needg:
-                    G[curG: curG + nPoint * useQ] = 2.0 * h * (weight * ((useX[:, Qcol] - lqrobj.xbase[Qcol]) * lqrobj.Q.data)).flatten()
+                    G_[:, :useQ] = 2.0 * h * (weight * ((useX[:, Qcol] - lqrobj.xbase[Qcol]) * lqrobj.Q.data))
                     if rec:
                         col_[:, :useQ] = Qcol + baseCol
                     curG += nPoint * useQ
             if useR > 0:
                 yR = np.dot(weight[:, 0], np.sum(((useU[:, Rcol] - lqrobj.ubase[Rcol]) ** 2) * lqrobj.R.data, axis=1)) * h
                 if needg:
-                    G[curG: curG + nPoint * useR] = 2.0 * h * (weight * ((useU[:, Rcol] - lqrobj.ubase[Rcol]) * lqrobj.R.data)).flatten()
+                    G_[:, useQ: useQ + useR] = (2.0 * h * (weight * ((useU[:, Rcol] - lqrobj.ubase[Rcol]) * lqrobj.R.data)))
                     if rec:
                         col_[:, useQ: useQ+useR] = Rcol + baseCol + self.dimx
                     curG += nPoint * useR
             if useP > 0:
-                yP = np.dot(weight[:, 0], lqrobj.P.data * np.sum((useP_[:, Pcol] - lqrobj.pbase[Pcol]) ** 2, axis=0)) * h
+                yP = np.dot(weight[:, 0], np.sum(((useP_[:, Pcol] - lqrobj.pbase[Pcol]) ** 2) * lqrobj.P.data, axis=1)) * h
                 if needg:
-                    G[curG: curG + nPoint * useP] = 2.0 * h * (weight * ((useP_[:, Pcol] - lqrobj.pbase[Pcol]) * lqrobj.P.data)).flatten()
+                    G_[:, useQ+useR: useQ+useR+useP] = (2.0 * h * (weight * ((useP_[:, Pcol] - lqrobj.pbase[Pcol]) * lqrobj.P.data)))
                     if rec:
                         col_[:, useQ+useR: useQ+useR+useP] = Pcol + baseCol + self.dimx + self.dimu
                     curG += nPoint * useP

trajOptLib/trajOptManifoldCollocationProblem.py
@@ -140,17 +140,23 @@ class trajOptManifoldCollocProblem(trajOptCollocProblem):
         self.numSol += self.numGamma  # add those values to optimization variables
         self.numSol += self.numAuxVc  # add those values for inconvenient un-repeat issue between linear and nonlinear Jacobian
 
-    def preProcess(self):
+    def preProcess(self, defect_u=True, defect_p=False):
         """Initialize the problem, allocating spaces.
 
         Compared iwth trajOptCollocProblem, it has new sets of variables, different constraints.
 
         """
         self.colloc_constr_is_on = False  # we never need this, actually
+        self.defectU = defect_u
+        self.defectP = defect_p
         numDyn = self.dimdyn * self.nPoint  # constraints from system dynamics, they are imposed everywhere
         dynDefectSize = 2 * self.daeOrder * self.dimdyn
         self.dynDefectSize = dynDefectSize
-        defectSize = dynDefectSize + self.dimu  # from q, dq, etc, and u are average, note that p is not averaged, it is free
+        defectSize = dynDefectSize
+        if defect_u:
+            defectSize += self.dimu
+        if defect_p:
+            defectSize += self.dimp
         self.defectSize = defectSize
         numDefectDyn = (self.N - 1) * defectSize  # from enforcing those guys
         self.numDefectDyn = numDefectDyn
@@ -191,7 +197,7 @@ class trajOptManifoldCollocProblem(trajOptCollocProblem):
         This overrides previous one by considering auxiliary Vc variables
 
         """
-        curRow, A, row, col = self.__setDefectPattern__(ndyncon, False)
+        curRow, A, row, col = self.__setDefectPattern__(ndyncon, self.defectU, self.defectP)
         curRow += nnonlincon
         # we are ready to parse linear constraints
         lstCA, lstCArow, lstCAcol = self.__parseLinearConstraints__(curRow)
@@ -203,6 +209,7 @@ class trajOptManifoldCollocProblem(trajOptCollocProblem):
         new_A_col = np.zeros((self.N - 1) * self.dimdyn * 2, dtype=int)
         accum_n = 0
         baseRange = np.arange(self.dimdyn)
+        # set A for auxiliary velocity variables
         for i in range(self.N - 1):
             new_A[accum_n: accum_n + self.dimdyn] = 1.0
             new_A[accum_n + self.dimdyn: accum_n + 2*self.dimdyn] = -1.0
@@ -339,80 +346,22 @@ class trajOptManifoldCollocProblem(trajOptCollocProblem):
         nc = self.man_constr.nc
         nf = self.man_constr.nf
         y = np.zeros(self.numF)
-        if self.grad:
-            self.__callg__(guess, y, np.zeros(1), np.zeros(1), np.zeros(1), False, False)
-        else:
-            self.__callf__(guess, y)
-        # parse dynamics constraints
-        dynCon = np.reshape(y[1:(2 * N - 1) * dimdyn + 1], (2 * N - 1, dimdyn))
-        curN = 1 + (2 * N - 1) * dimdyn
-        curNf = curN + self.numDefectDyn  # this is actually wrong since dyn comes before u/p
-        defectCon = np.reshape(y[curN: curNf], (N - 1, -1))
-        curN = curNf
-        pointCon = []
-        for constr in self.pointConstr:
-            pointCon.append(y[curN: curN + constr.nf])
-            curN += constr.nf
-        pathCon = []
-        for constr in self.pathConstr:
-            if self.colloc_constr_is_on:
-                useN = nPoint
-            else:
-                useN = N
-            pathCon.append(np.reshape(y[curN: curN + useN * constr.nf], (useN, constr.nf)))
-            curN += useN * constr.nf
-        nonLinCon = []
-        for constr in self.nonLinConstr:
-            nonLinCon.append(y[curN: curN + constr.nf])
-            curN += constr.nf
+
+        # call previous function
+        rst = trajOptCollocProblem.parseF(self, guess, y)
+
+        numC, nnonlincon, nlincon = self.__sumConstrNum__()
+        curN = 1 + (2 * N - 1) * dimdyn + self.numDefectDyn + nnonlincon
+
         manCon = np.reshape(y[curN: curN + N*nf], (N, nf))
-        # check bounds, return a -1, 1 value for non-equality bounds, and 0 for equality bounds
-        useX, useU, useP = self.__parseX__(guess)
-        Xbound = checkInBounds(useX[:, :dimx], self.xbd)
-        x0bound = checkInBounds(useX[0, :dimx], self.x0bd)
-        xfbound = checkInBounds(useX[-1, :dimx], self.xfbd)  # TODO: support acceleration bound check
-        ubound = checkInBounds(useU, self.ubd)
-        if self.dimp > 0:
-            pbound = checkInBounds(useP, self.pbd)
-        else:
-            pbound = None
-        if self.t0ind > 0:
-            t0bound = checkInBounds(guess[self.t0ind], self.t0)
-        else:
-            t0bound = None
-        if self.tfind > 0:
-            tfbound = checkInBounds(guess[self.tfind], self.tf)
-        else:
-            tfbound = None
-        if self.lenAddX > 0:
-            addx = self.__parseAddX__(guess)
-            addXbound = [checkInBounds(addx_, [addx__.lb, addx__.ub]) for addx_, addx__ in zip(addx, self.addX)]
-        else:
-            addXbound = None
+        rst['man'] = manCon
+        # we ignore linear and auxVc constraints
 
         useGamma = self.__parseGamma__(guess)
         useAuxVc = self.__parseAuxVc__(guess)
+        rst['gamma'] = useGamma
+        rst['useVc'] = useAuxVc
 
-        # get values of objective functions
-        obj = guess[self.numSol - self.objaddn:]
-
-        rst = {'obj': obj, 'dyn': dynCon, 'defect': defectCon, 'point': pointCon, 'path': pathCon, 'nonlin': nonLinCon,
-               'Xbd': Xbound, 'Ubd': ubound, 'x0bd': x0bound, 'xfbd': xfbound, 'Pbd': pbound, 'man': manCon,
-               't0bd': t0bound, 'tfbd': tfbound, 'addXbd': addXbound,
-               'X': useX, 'U': useU, 'P': useP, 'Gamma': useGamma, 'useVc': useAuxVc}
-        if self.t0ind > 0:
-            rst['t0'] = guess[self.t0ind]
-        else:
-            rst['t0'] = self.t0
-        if self.tfind > 0:
-            rst['tf'] = guess[self.tfind]
-        else:
-            rst['tf'] = self.tf
-        # parse addx
-        if self.lenAddX > 0:
-            addx = self.__parseAddX__(guess)
-            for i, addx_ in enumerate(addx):
-                rst['addx_%d' % i] = addx_
         return rst
 
     def __parseGamma__(self, x):
@@ -438,8 +387,8 @@ class trajOptManifoldCollocProblem(trajOptCollocProblem):
         # we only need to set gamma which are unbounded
         curN = self.numTraj + self.lenAddX
         # gamma cannot be too large, I guess
-        self.batchSetXlb(-1e1*np.ones(self.numGamma), curN)
-        self.batchSetXub(1e1*np.ones(self.numGamma), curN)
+        self.batchSetXlb(-1e20*np.ones(self.numGamma), curN)
+        self.batchSetXub(1e20*np.ones(self.numGamma), curN)
         curN += self.numGamma
         finalN = self.numSol
         self.batchSetXlb(-1e20*np.ones(finalN - curN), curN)